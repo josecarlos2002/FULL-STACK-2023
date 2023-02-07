9b1afa797464a8026f789675fae6b4ed3692411d
josecarlos2002 <123936016+josecarlos2002@users.noreply.github.com>
2023-02-07 09:12:59 +0100

    commit 6/2/2023

---
+++ JAVA SCRIPT/main.js
@@ -0,0 +1,7 @@
+console.log('Hello world!');
+console.log('hola');
+document.write("Texto2");
+console.log('en', 'un', 'lugar', 'de', 'la', 'Mancha');
+console.info('Info');
+console.warn('Warning');
+console.error('Error');
---
+++ JAVA SCRIPT/variables.js
@@ -0,0 +1,16 @@
+var alert1 = 5;
+console.log(alert1)
+var alert1 = '';
+
+// permitido
+
+let alert2 = 5;
+console.log(alert2)
+// no permitido porque ya se declaró antes
+let alert2 = 5;
+
+
+alert2=15
+const alert3 = 13
+console.log(alert1);
+console.log(alert2);
